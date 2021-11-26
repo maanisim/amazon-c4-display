2463af625469469e9e77608c69fefb355370bd97
michalanisimow <M.anisimow@liverpool.ac.uk>
2021-11-26 00:34:14 +0000

    v1

---
+++ main.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name        Amazon camelcamelcamel Chart
+// @namespace   amazon.co.uk
+// @version     1
+// @description Displays a camelcamelcamel past price performance chart directly on the amazon.co.uk
+// @author      https://github.com/michalani/daft.ie-rent-conventer
+// @license     MIT
+// @include     https://www.amazon.co.uk/*/dp/*
+// @include     https://www.amazon.co.uk/dp/*
+// @grant       none
+// @noframes
+// ==/UserScript==
+
+function main(){
+    //parse the produc id of on the amazon (https://www.amazon.co.uk/*/dp/B07GDR2PH9) where B07GDR2PH9 would be product id
+    let currentURL = window.location.href;
+    let productID = currentURL.split("/dp/")[1].split("?")[0];
+
+    //fetch image from the camelcamelcamel
+    var img = document.createElement("img");
+    //img.setAttribute("id", "priceTrackingImage");
+
+    //amazon and 3rd parties
+    img.src = "https://charts.camelcamelcamel.com/uk/"+productID+"/amazon-new.png?force=1&zero=0&w=855&h=513&desired=false&legend=1&ilt=1&tp=all&fo=0&lang=en";
+
+    //only amazon
+    //img.src = "https://charts.camelcamelcamel.com/uk/"+productID+"/amazon.png?force=1&zero=0&w=945&h=567&desired=false&legend=1&ilt=1&tp=all&fo=0&lang=en";
+
+    //wher eto append the chart
+    let src = document.getElementById("leftCol");
+    src.appendChild(img);
+
+    //make the chart clickable directly to the camelcamelcamel page.
+    img.addEventListener("click", function() {
+        window.location.replace("https://uk.camelcamelcamel.com/product/"+productID);
+    })
+}
+
+main();
